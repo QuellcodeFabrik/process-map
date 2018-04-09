07bd5eae1b7abced2b7df8f67787a8e819b5de95
Sascha Gros <cin.dev-46@mercedes-benz.io>
2018-04-09 17:20:41 +0200

    Add another test

--- tests/unit/ProcessStep.spec.ts
+++ tests/unit/ProcessStep.spec.ts
@@ -1,21 +1,32 @@
 import { expect } from 'chai';
-import { shallow } from '@vue/test-utils';
+import { mount, shallow } from '@vue/test-utils';
 import { ProcessStep } from '@/contracts';
 import ProcessStepComponent from '@/components/ProcessStep.vue';
 
-describe('ProcessStep.vue', () => {
-  it('renders the step properties when passed', () => {
-    const step: ProcessStep = {
-      id: 'id-01',
-      position: '1',
-      title: 'Test step',
-      label: 'Non-empty label',
-      url: 'http://www.google.com',
-      showOnMap: true
-    };
-    const parallelSteps = false;
-    const $log = { debug: () => {}, info: () => {}, error: () => {} };
+describe('ProcessStep component', () => {
+  const step: ProcessStep = {
+    id: 'id-01',
+    position: '1',
+    title: 'Test step',
+    label: 'Non-empty label',
+    url: 'http://www.google.com',
+    showOnMap: true
+  };
+  const parallelSteps = false;
+  const $log = { debug: () => {}, info: () => {}, error: () => {} };
 
+  it('should set the initial component properties', () => {
+    const wrapper: any = mount(ProcessStepComponent, {
+      mocks: {
+        $log
+      },
+      propsData: { step, parallelSteps } });
+
+    expect(wrapper.vm.showSubProcess).to.equal(false);
+    expect(wrapper.vm.showHeader).to.equal(true);
+  });
+
+  it('should render the properties being passed from the parent', () => {
     const wrapper = shallow(ProcessStepComponent, {
       mocks: {
         $log
